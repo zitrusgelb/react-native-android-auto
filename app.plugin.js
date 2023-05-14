c7f2bdfc2a7c504c57a0669c601cf9807284eb64
Sven van der Zwet <48092471+JanSneeuw@users.noreply.github.com>
2023-05-14 21:55:16 +0200

    Added a config plugin so it can be ran in a expo managed dev client

---
+++ app.plugin.js
@@ -0,0 +1,60 @@
+const { withAndroidManifest, withDangerousMod, AndroidConfig } = require('@expo/config-plugins');
+
+const withAutomotiveApp = (config) => {
+    config = withAndroidManifest(config, async (config) => {
+        let mainApplication = AndroidConfig.Manifest.getMainApplicationOrThrow(config.modResults);
+
+        // Adding meta-data
+        mainApplication['meta-data'] = (mainApplication['meta-data'] || []).concat({
+            $: {
+                'android:name': "com.google.android.gms.car.application",
+                'android:resource': "@xml/automotive_app_desc",
+            }
+        });
+
+        // Adding service
+        mainApplication.service = (mainApplication.service || []).concat({
+            $: {
+                'android:name': "com.shopify.rnandroidauto.CarService",
+                'android:exported': "true",
+            },
+            'intent-filter': [{
+                action: [{
+                    $: {
+                        'android:name': "com.google.android.car.action.CAR_APP",
+                    }
+                }]
+            }]
+        });
+
+        return config;
+    });
+
+    // Adding automotive_app_desc.xml file
+    config = withDangerousMod(config, [
+        'android',
+        async (config) => {
+            const appDir = config.modRequest.projectRoot + '/android/app/src/main/res/xml/';
+            const filename = appDir + 'automotive_app_desc.xml';
+
+            const fs = require('fs');
+
+            if (!fs.existsSync(appDir)) {
+                fs.mkdirSync(appDir, { recursive: true });
+            }
+
+            fs.writeFileSync(filename,
+                `<?xml version="1.0" encoding="utf-8"?>
+         <automotiveApp>
+           <uses name="template" />
+         </automotiveApp>`
+            );
+
+            return config;
+        }
+    ]);
+
+    return config;
+};
+
+module.exports = withAutomotiveApp;
